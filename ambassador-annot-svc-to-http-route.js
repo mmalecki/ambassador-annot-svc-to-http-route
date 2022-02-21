3fa96f8e6c1eb657cb86132452e82a6dd2fd3d48
Maciej Małecki <me@mmalecki.com>
2022-02-21 13:21:35 +0100

    Implement rudimentary conversion

---
+++ ambassador-annot-svc-to-http-route.js
@@ -0,0 +1,42 @@
+'use strict'
+const yaml = require('js-yaml')
+
+module.exports = function (svc) {
+  if (!svc.kind === 'Service') throw new TypeError(`Expected \`kind: Service\`, received \`kind: ${svc.kind}\``)
+  const annot = svc?.metadata?.annotations?.['getambassador.io/config']
+  if (!annot) return null
+
+  const mapping = yaml.loadAll(annot)[0]
+
+  if (mapping.kind !== 'Mapping') return null
+
+  return {
+    apiVersion: 'gateway.networking.k8s.io/v1alpha2',
+    kind: 'HTTPRoute',
+    metadata: {
+      namespace: svc.metadata.namespace,
+    },
+    spec: {
+      parentRefs: [],
+      rules: [
+        {
+          backendRefs: [
+            {
+              kind: 'Service',
+              name: svc.metadata.name,
+              port: svc.spec.ports[0].port
+            }
+          ],
+          matches: [
+            {
+              path: {
+                type: 'PathPrefix',
+                value: mapping.prefix
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
